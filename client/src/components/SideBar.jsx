480822833ef503ca1284b2032c5e8071557e3d2c
Karthik P <padiyil2004@gmail.com>
2025-12-03 16:47:47 +0530

    completed the basic layout

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const SideBar = () => {
+const SideBar = ({sidebarOpen,setSideBarOpen}) => {
   return (
-    <div>
+    <div className={`w-60 flex flex-col justify-between items-center border-r bg-white border-gray-200 shadow-md ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'} transition-all duration-300 ease-in-out`}>
       
     </div>
   )
