f400f2986aa280f785de8f6b24d77f1f62d1386b
Karthik P <padiyil2004@gmail.com>
2025-12-11 16:21:40 +0530

    completed the frontend of the create-post page

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,10 +1,67 @@
-import React from 'react'
+import React, {  useState } from 'react'
+import { dummyUserData } from '../assets/assets';
+import { Image, X } from 'lucide-react';
+import { toast } from 'react-hot-toast';
 
 const CreatePost = () => {
+  const [content,setContent] = useState('');
+  const [images,setImages] = useState([]);
+  const [loading,setLoading] = useState(false);
+  const user = dummyUserData;
+
+  const handleSubmit = async() => {
+
+  }
+
   return (
-    <div>
-      
+  <div className='min-h-screen bg-gradient-to-b from-slate-50 to-white'>
+    <div className='max-w-6xl mx-auto p-6'>
+      {/* Title */}
+      <div className='mb-8'>
+        <h1 className='text-3xl font-bold text-slate-900 mb-2'>Create Post</h1>
+        <p className='text-slate-600'>Share your thoughts with the world</p>
+      </div>
+      <div className='max-w-xl bg-white p-4 rounded-xl shadow-md space-y-4'>
+        <div>
+          <div className='flex gap-1 items-center'>
+            <img src={user.profile_picture} className='rounded-full size-12 shadow'/>
+            <div className='ml-2'>
+              <h2 className='font-semibold'>{user.full_name}</h2>
+              <p className='text-sm text-gray-500'>@{user.username}</p>
+            </div>
+          </div>
+          <textarea className='w-full resize-none max-h-30 mt-6 text-sm outline-none placeholder-gray-400' placeholder='Whats happening ?' onChange={(e) => setContent(e.target.value)} value={content}/>
+          {
+            images.length > 0 && (
+              <div className='flex flex-wrap gap-2 mt-4'>
+                {images.map((image,i)=>(
+                  <div key={i} className='relative group'>
+                    <img src={URL.createObjectURL(image)} className='h-20 rounded-md'/>
+                    <div onClick={()=>setImages(images.filter((_,index)=> index!== i))} className='absolute hidden group-hover:flex justify-center items-center top-0 right-0 bottom-0 left-0 bg-black/40 rounded-md cursor-pointer'>
+                    <X className='size-6 text-white' />  
+                    </div>
+                  </div>
+                ))}
+              </div>
+            ) 
+          }
+          <div className='flex items-center mt-3 justify-between pt-3 border-t border-gray-300'>
+            <label htmlFor='images' className='flex items-center gap-2 text-sm text-gray-500 hover:text-gray-700 transition cursor-pointer'>
+              <Image className='size-5' />
+            </label>
+            <input type='file' id='images' className='hidden' accept='image/*' multiple onChange={(e) => setImages([...images,...e.target.files])} />
+            <button disabled={loading} className='text-sm bg-gradient-to-r from-indigo-500 to-purple-600 hover:from-indigo-600 hover:to-purple-700 active:scale-95 transition text-white font-medium px-8 py-2 rounded-md cursor-pointer hover:scale-105 duration-150' onClick={()=>toast.promise(handleSubmit(),{
+              loading: 'Publishing post...',
+              success: 'Post added!',
+              error: 'Post not added.'
+            })}>
+              Publish Post
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
   )
 }
 
