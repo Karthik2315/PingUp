73f0957730b9f4a04969be9618d371aa186a2ad5
Karthik P <padiyil2004@gmail.com>
2025-12-12 23:13:30 +0530

    Added inngest basic functions

--- server/server.js
+++ server/server.js
@@ -3,18 +3,19 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import 'dotenv/config';
 import connectDB from './config/db.js';
+import {inngest,functions} from '/inngest/index.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+const PORT = process.env.PORT || 5000;
+await connectDB();
 
 app.get('/', (req, res) => {
   res.send('Hello from PingUp server!');
 });
-
-const PORT = process.env.PORT || 5000;
-await connectDB();
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
