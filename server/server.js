cf78511ed51c1de49c49204c607589fb895280e2
Karthik P <padiyil2004@gmail.com>
2025-12-13 00:27:51 +0530

    added the clerkmiddlware in server.js

--- server/server.js
+++ server/server.js
@@ -5,14 +5,16 @@ import 'dotenv/config';
 import connectDB from './config/db.js';
 import {inngest,functions} from './inngest/index.js';
 import { serve } from "inngest/express";
+import { clerkMiddleware } from '@clerk/express'
+
 
 const app = express();
+await connectDB();
 
 app.use(express.json());
 app.use(cors());
 const PORT = process.env.PORT || 5000;
-await connectDB();
-
+app.use(clerkMiddleware());
 app.get('/', (req, res) => {
   res.send('Hello from PingUp server!');
 });
