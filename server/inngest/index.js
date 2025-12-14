63b7f1b09f0e6b9be437fe932ee3c3dd6f4da3a6
Karthik P <padiyil2004@gmail.com>
2025-12-14 18:26:02 +0530

    small correction on email remainder

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,7 +1,7 @@
 import { Inngest } from "inngest";
 import User from "../models/UserModel.js";
-import { Connection } from "mongoose";
 import sendEmail from "../config/nodeMailer.js";
+import Connection from "../models/ConnectionModel.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "pingUp-app" });
