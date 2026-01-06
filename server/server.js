47608804f87a4d3055a2409d1c57063184fa250c
Karthik P <padiyil2004@gmail.com>
2026-01-06 18:52:37 +0530

    completed postRouter

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ import {inngest,functions} from './inngest/index.js';
 import { serve } from "inngest/express";
 import { clerkMiddleware } from '@clerk/express'
 import userRouter from './routes/userRouter.js';
+import postRouter from './routes/postRouter.js';
 
 
 const app = express();
@@ -21,6 +22,7 @@ app.get('/', (req, res) => {
 });
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/user",userRouter);
+app.use("/api/post",postRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
