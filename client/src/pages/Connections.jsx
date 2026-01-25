607d0e35de7fc90e9c6a9eb4db759d6a43aaacc0
Karthik P <padiyil2004@gmail.com>
2026-01-25 21:19:48 +0530

    connected the backend functions

--- client/src/pages/Connections.jsx
+++ client/src/pages/Connections.jsx
@@ -1,13 +1,18 @@
-import React, { useState } from 'react'
-import { data, useNavigate } from 'react-router-dom'
-import { dummyConnectionsData, dummyFollowersData, dummyFollowingData, dummyPendingConnectionsData } from '../assets/assets'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { MessagesSquare, UserCheck, UserPlus, UserRoundPen, Users } from 'lucide-react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useAuth } from '@clerk/clerk-react';
+import { fetchConnections } from '../features/connections/connectionSlice';
+import api from '../api/axios';
+import toast from 'react-hot-toast';
 
 const Connections = () => {
-  const connections = dummyConnectionsData;
-  const followers = dummyFollowersData;
-  const following = dummyFollowingData;
-  const pending = dummyPendingConnectionsData;
+  const connections = useSelector((state) => state.connections.connections);
+  const followers = useSelector((state) => state.connections.followers);
+  const following = useSelector((state) => state.connections.following);
+  const pending = useSelector((state) => state.connections.pendingConnections);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const dataArray = [
     { label: 'Followers', value: followers, icon: Users },
@@ -16,6 +21,46 @@ const Connections = () => {
     { label: 'Connections', value: connections, icon: UserPlus },
   ];
   const [currentTab,setCurrentTab] = useState('Followers')
+  const {getToken} = useAuth();
+  const handleUnfollow = async(userId) => {
+    try {
+      const token = await getToken();
+      const {data} = await api.post('api/user/unfollow',{id:userId},{
+        headers:{
+          Authorization:`Bearer ${token}`
+        }
+      })
+      if(data.success){
+        toast.success("Unfollowed user successfully");
+        dispatch(fetchConnections(token));
+      }else{
+        console.log(data.message);
+      }
+    } catch (error) {
+      console.error("Error unfollowing user:", error);
+    }
+  }
+  const acceptConnection = async(userId) => {
+    try {
+      const token = await getToken();
+      const {data} = await api.post('api/user/accept',{id:userId},{
+        headers:{
+          Authorization:`Bearer ${token}`
+        }
+      })
+      if(data.success){
+        toast.success("Accepted user successfully");
+        dispatch(fetchConnections(token));
+      }else{
+        console.log(data.message);
+      }
+    } catch (error) {
+      console.error("Error unfollowing user:", error);
+    }
+  }
+  useEffect(()=>{
+    getToken().then((token) => dispatch(fetchConnections(token)));
+  },[])
   return (
     <div className='min-h-screen bg-slate-50'>
       <div className='max-w-6xl mx-auto p-6'>
@@ -51,14 +96,14 @@ const Connections = () => {
                   <button className='w-full p-2 text-sm rounded-md bg-gradient-to-r from-indigo-500 to-purple-600 hover:from-indigo-600 hover:to-purple-700 active:scale-95 transition text-white cursor-pointer' onClick={() => navigate(`/profile/${user._id}`)}>View Profile</button>
                   {
                     currentTab === 'Following' && (
-                      <button className='w-full p-2 text-sm rounded bg-slate-100 hover:bg-slate-200 text-black active:scale-95 transition cursor-pointer'>
+                      <button onClick={() => handleUnfollow(user._id)} className='w-full p-2 text-sm rounded bg-slate-100 hover:bg-slate-200 text-black active:scale-95 transition cursor-pointer'>
                         Unfollow
                       </button>
                     )
                   }
                   {
                     currentTab === 'Pending' && (
-                      <button className='w-full p-2 text-sm rounded bg-slate-100 hover:bg-slate-200 text-black active:scale-95 transition cursor-pointer'>
+                      <button onClick={() => acceptConnection(user._id)} className='w-full p-2 text-sm rounded bg-slate-100 hover:bg-slate-200 text-black active:scale-95 transition cursor-pointer'>
                         Accept
                       </button>
                     )
