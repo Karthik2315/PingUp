de877e1b70270f7b08c1d1ceafedcb6fba1a4741
Karthik P <padiyil2004@gmail.com>
2026-01-27 18:12:18 +0530

    added the reducer functions

--- client/src/features/messages/messageSlice.js
+++ client/src/features/messages/messageSlice.js
@@ -1,14 +1,39 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import api from "../../api/axios";
 
 const initialState = {
   messages: []
 }
 
+export const fetchMessages = createAsyncThunk('messages/fetchMessages',async({token,id})=>{
+  const {data} = await api.post('/api/messages/get',{to_user_id:id},{
+    headers:{
+      Authorization:`Bearer ${token}`
+    }
+  });
+  return data.success ? data:null;
+});
+
 const messagesSlice = createSlice({
   name:"messages",
   initialState,
   reducers:{
-
+    setMessages: (state,action)=>{
+      state.messages= action.payload
+    },
+    addMessages: (state,action)=>{
+      state.messages = [...state.messages,action.payload];
+    },
+    resetMessages: (state)=>{
+      state.messages = [];
+    }
+  },
+  extraReducers: (builder)=>{
+    builder.addCase(fetchMessages.fulfilled,(state,action)=>{
+      if(action.payload){
+        state.messages = action.payload.messages;
+      }
+    })
   }
 });
 
